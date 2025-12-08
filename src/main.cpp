f36a23ca0340cab553f64138c503af4c3763b861
Jlesster <jns.corey@gmail.com>
2025-12-08 21:32:37 +0000

    sdl baseline for program
    necassary build structure for creating a graphical rendered window

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_init.h>
+#include <SDL3/SDL_main.h>
+
+SDL_Window* window;
+
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
+
+}
+
+SDL_AppResult SDL_AppIterate(void **appstate, int argc, char **argv) {
+
+}
+
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
+
+}
+
+void SDL_AppQuit(void *appstate, SDL_AppResult result) {
+
+}
