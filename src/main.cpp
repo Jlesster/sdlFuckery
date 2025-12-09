60a8abac77d097826cc38547161cac85242ddf4a
Jlesster <jns.corey@gmail.com>
2025-12-09 01:07:53 +0000

    un fucked

--- src/main.cpp
+++ src/main.cpp
@@ -22,19 +22,40 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
   device = SDL_CreateGPUDevice(SDL_GPU_SHADERFORMAT_SPIRV, false, NULL);
   SDL_ClaimWindowForGPUDevice(device, window);
 
+  return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppIterate(void **appstate, int argc, char **argv) {
   //creating command buffer (gpu command array)
   cmd = SDL_AcquireGPUCommandBuffer(device);
 
   //get the swapchain texture
   SDL_WaitAndAcquireGPUSwapchainTexture(cmd, window, &swapTexture, &width, &height);
 
+  //end fram early if no swapchain texture
+  if(swapTexture == NULL) {
+    //always submint the command buffer after new instructions
+    SDL_SubmitGPUCommandBuffer(cmd);
+    return SDL_APP_CONTINUE;
+  }
+
+  //create the color target
+  SDL_GPUColorTargetInfo colorTargetInfo{};
+  colorTargetInfo.clear_color = {240/255.0f, 240/255.0f, 240/255.0f, 255/255.0f};
+  colorTargetInfo.load_op = SDL_GPU_LOADOP_CLEAR;
+  colorTargetInfo.store_op = SDL_GPU_STOREOP_STORE;
+  colorTargetInfo.texture = swapTexture;
+
+  //begin render pass
+  renderPass = SDL_BeginGPURenderPass(cmd, &colorTargetInfo, 1, NULL);
+
+  //drawing
+
+  //end render pass
+  SDL_EndGPURenderPass(renderPass);
+
   //sending command buffer to gpu
   SDL_SubmitGPUCommandBuffer(cmd);
-
-  return SDL_APP_CONTINUE;
-}
-
-SDL_AppResult SDL_AppIterate(void **appstate, int argc, char **argv) {
 
   return SDL_APP_CONTINUE;
 }
