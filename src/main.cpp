2f27dde2f019e97c5902774b96e394af32457422
Jlesster <jns.corey@gmail.com>
2025-12-09 00:56:45 +0000

    minor texture stuff
    nearly at rendering a color to the window and in my case rendering a
    functioning window

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,35 @@
+#include <SDL3/SDL_gpu.h>
 #define SDL_MAIN_USE_CALLBACKS
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_init.h>
 #include <SDL3/SDL_main.h>
 #include <SDL3/SDL_video.h>
 
-SDL_Window *window;
+//init window and gpu
+SDL_Window* window;
+SDL_GPUDevice* device;
+SDL_GPUCommandBuffer* cmd;
+SDL_GPUTexture* swapTexture;
+SDL_GPURenderPass* renderPass;
+
+Uint32 width, height;
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
+  //creating window
   window = SDL_CreateWindow("Hello maybe triangle", 720, 480, 0);
+
+  //creating device
+  device = SDL_CreateGPUDevice(SDL_GPU_SHADERFORMAT_SPIRV, false, NULL);
+  SDL_ClaimWindowForGPUDevice(device, window);
+
+  //creating command buffer (gpu command array)
+  cmd = SDL_AcquireGPUCommandBuffer(device);
+
+  //get the swapchain texture
+  SDL_WaitAndAcquireGPUSwapchainTexture(cmd, window, &swapTexture, &width, &height);
+
+  //sending command buffer to gpu
+  SDL_SubmitGPUCommandBuffer(cmd);
 
   return SDL_APP_CONTINUE;
 }
@@ -25,5 +47,7 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
 }
 
 void SDL_AppQuit(void *appstate, SDL_AppResult result) {
+  //destroying device and window
+  SDL_DestroyGPUDevice(device);
   SDL_DestroyWindow(window);
 }
