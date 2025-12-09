ee339397928c6c941a4847469701e042ca483f23
Jlesster <jns.corey@gmail.com>
2025-12-09 23:51:28 +0000

    some baseline code for me to compare and debug mine to

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -1,9 +1,9 @@
-#version 460
+#version 450
 
-layout (location = 0) in vec4 v_color;
-layout (location = 0) out vec4 FragColor;
+layout(location = 0) in vec4 v_color;
 
-void main()
-{
-    FragColor = v_color;
+layout(location = 0) out vec4 outColor;
+
+void main() {
+    outColor = v_color;
 }
