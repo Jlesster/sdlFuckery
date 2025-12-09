2a55f468f216bb82f6370b19540de7420b796d93
Jlesster <jns.corey@gmail.com>
2025-12-09 01:48:55 +0000

    DISCO WINDOW

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,19 @@
-#include <SDL3/SDL_gpu.h>
+#include <SDL3/SDL_stdinc.h>
+#include <SDL3/SDL_timer.h>
 #define SDL_MAIN_USE_CALLBACKS
 #include <SDL3/SDL.h>
+#include <SDL3/SDL_gpu.h>
 #include <SDL3/SDL_init.h>
 #include <SDL3/SDL_main.h>
 #include <SDL3/SDL_video.h>
 
 //init window and gpu
+Uint32 width, height;
 SDL_Window* window;
 SDL_GPUDevice* device;
-SDL_GPUCommandBuffer* cmd;
 SDL_GPUTexture* swapTexture;
 SDL_GPURenderPass* renderPass;
-
-Uint32 width, height;
+SDL_GPUCommandBuffer* cmd;
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
   //creating window
@@ -25,9 +26,10 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
   return SDL_APP_CONTINUE;
 }
 
-SDL_AppResult SDL_AppIterate(void **appstate, int argc, char **argv) {
+SDL_AppResult SDL_AppIterate(void *appstate) {
   //creating command buffer (gpu command array)
   cmd = SDL_AcquireGPUCommandBuffer(device);
+
 
   //get the swapchain texture
   SDL_WaitAndAcquireGPUSwapchainTexture(cmd, window, &swapTexture, &width, &height);
@@ -39,9 +41,16 @@ SDL_AppResult SDL_AppIterate(void **appstate, int argc, char **argv) {
     return SDL_APP_CONTINUE;
   }
 
-  //create the color target
+  //get ticks and convert from ms to s
+  const double tick = ((double)SDL_GetTicks()) / 1000.0;
+  //make sine wave between each color using tick as basis
+  const float red = (float) (0.7 + 0.7 * SDL_sin(tick));
+  const float blue = (float) (0.7 + 0.7 * SDL_sin(tick + SDL_PI_D * 2 / 3));
+  const float green = (float) (0.7 + 0.7 * SDL_sin(tick + SDL_PI_D * 4 / 3));
+
+  //create the color target                                     1
   SDL_GPUColorTargetInfo colorTargetInfo{};
-  colorTargetInfo.clear_color = {240/255.0f, 240/255.0f, 240/255.0f, 255/255.0f};
+  colorTargetInfo.clear_color = {red, blue, green, 255/255.0f};
   colorTargetInfo.load_op = SDL_GPU_LOADOP_CLEAR;
   colorTargetInfo.store_op = SDL_GPU_STOREOP_STORE;
   colorTargetInfo.texture = swapTexture;
@@ -72,3 +81,4 @@ void SDL_AppQuit(void *appstate, SDL_AppResult result) {
   SDL_DestroyGPUDevice(device);
   SDL_DestroyWindow(window);
 }
+
