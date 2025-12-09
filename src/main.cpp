e0a744fef29e6a96038c74f08810523ab7dd4df2
Jlesster <jns.corey@gmail.com>
2025-12-09 19:25:45 +0000

    "theres some fuckery afoot"

--- src/main.cpp
+++ src/main.cpp
@@ -1,69 +1,78 @@
-#include <SDL3/SDL_stdinc.h>
-#include <SDL3/SDL_timer.h>
-#define SDL_MAIN_USE_CALLBACKS
-#include <SDL3/SDL.h>
+1iinclude <SDL3/SDL_events.h>
 #include <SDL3/SDL_gpu.h>
-#include <SDL3/SDL_init.h>
-#include <SDL3/SDL_main.h>
+#include <SDL3/SDL_oldnames.h>
 #include <SDL3/SDL_video.h>
+#define SDL_MAIN_USE_CALLBACKS
+#include <SDL3/SDL_main.h>
+#include <SDL3/SDL.h>
 
-//init window and gpu
-Uint32 width, height;
 SDL_Window* window;
-SDL_GPUDevice* device;
-SDL_GPUTexture* swapTexture;
-SDL_GPURenderPass* renderPass;
 SDL_GPUCommandBuffer* cmd;
+SDL_GPUDevice* device;
+SDL_GPURenderPass* rPass;
+SDL_GPUCopyPass* cPass;
+SDL_GPUTransferBuffer* transferBuffer;
+SDL_GPUTexture* sTexture;
+SDL_GPUBuffer* vBuffer;
+Uint32 width, height;
+
+const int wWidth = 720;
+const int wHeight = 480;
+
+struct vertex {
+  float x, y, z;
+  float r, g, b, a;
+};
+
+static vertex vertices[]
+{
+    {0.0f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f},     // top vertex
+    {-0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f},   // bottom left vertex
+    {0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f}     // bottom right vertex
+};
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
-  //creating window
-  window = SDL_CreateWindow("Hello maybe triangle", 720, 480, 0);
-
-  //creating device
+  window = SDL_CreateWindow("Base", wWidth, wHeight, SDL_WINDOW_RESIZABLE);
   device = SDL_CreateGPUDevice(SDL_GPU_SHADERFORMAT_SPIRV, false, NULL);
+  cPass = SDL_BeginGPUCopyPass(cmd);
+
+  SDL_GPUBufferCreateInfo bufferInfo{};
+  bufferInfo.size = sizeof(vertices);
+  bufferInfo.usage = SDL_GPU_BUFFERUSAGE_VERTEX;
+  vBuffer = SDL_CreateGPUBuffer(device, &bufferInfo);
+
+  SDL_GPUTransferBufferCreateInfo transferInfo{};
+  transferInfo.size = sizeof(vertices);
+  transferInfo.usage = SDL_GPU_TRANSFERBUFFERUSAGE_UPLOAD;
+  transferBuffer = SDL_CreateGPUTransferBuffer(device, &transferInfo);
+
   SDL_ClaimWindowForGPUDevice(device, window);
 
   return SDL_APP_CONTINUE;
 }
 
 SDL_AppResult SDL_AppIterate(void *appstate) {
-  //creating command buffer (gpu command array)
   cmd = SDL_AcquireGPUCommandBuffer(device);
+  SDL_WaitAndAcquireGPUSwapchainTexture(cmd, window, &sTexture, &width, &height);
 
-
-  //get the swapchain texture
-  SDL_WaitAndAcquireGPUSwapchainTexture(cmd, window, &swapTexture, &width, &height);
-
-  //end fram early if no swapchain texture
-  if(swapTexture == NULL) {
-    //always submint the command buffer after new instructions
+  if(window == NULL) {
     SDL_SubmitGPUCommandBuffer(cmd);
-    return SDL_APP_CONTINUE;
+    return SDL_APP_SUCCESS;
   }
 
-  //get ticks and convert from ms to s
-  const double tick = ((double)SDL_GetTicks()) / 1000.0;
-  //make sine wave between each color using tick as basis
-  const float red = (float) (0.7 + 0.7 * SDL_sin(tick));
-  const float blue = (float) (0.7 + 0.7 * SDL_sin(tick + SDL_PI_D * 2 / 3));
-  const float green = (float) (0.7 + 0.7 * SDL_sin(tick + SDL_PI_D * 4 / 3));
+  const double tick = (double)(SDL_GetTicks()) / 1000;
+  const float red = (float)(0.9 + 0.9 * SDL_sin(tick));
+  const float blue = (float)(0.9 + 0.9 * SDL_sin(tick + SDL_PI_F * 2 / 3));
+  const float green = (float)(0.9 + 0.9 * SDL_sin(tick + SDL_PI_F * 4 / 3));
 
-  //create the color target                                     1
-  SDL_GPUColorTargetInfo colorTargetInfo{};
-  colorTargetInfo.clear_color = {red, blue, green, 255/255.0f};
-  colorTargetInfo.load_op = SDL_GPU_LOADOP_CLEAR;
-  colorTargetInfo.store_op = SDL_GPU_STOREOP_STORE;
-  colorTargetInfo.texture = swapTexture;
+  SDL_GPUColorTargetInfo cTI{};
+  cTI.clear_color = {red, blue, green, 255/255.0f};
+  cTI.load_op = SDL_GPU_LOADOP_CLEAR;
+  cTI.store_op = SDL_GPU_STOREOP_STORE;
+  cTI.texture = sTexture;
 
-  //begin render pass
-  renderPass = SDL_BeginGPURenderPass(cmd, &colorTargetInfo, 1, NULL);
-
-  //drawing
-
-  //end render pass
-  SDL_EndGPURenderPass(renderPass);
-
-  //sending command buffer to gpu
+  rPass = SDL_BeginGPURenderPass(cmd, &cTI, 1, NULL);
+  SDL_EndGPURenderPass(rPass);
   SDL_SubmitGPUCommandBuffer(cmd);
 
   return SDL_APP_CONTINUE;
@@ -73,12 +82,13 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
   if(event->type == SDL_EVENT_WINDOW_CLOSE_REQUESTED) {
     return SDL_APP_SUCCESS;
   }
+
   return SDL_APP_CONTINUE;
 }
 
 void SDL_AppQuit(void *appstate, SDL_AppResult result) {
-  //destroying device and window
-  SDL_DestroyGPUDevice(device);
   SDL_DestroyWindow(window);
+  SDL_DestroyGPUDevice(device);
+  SDL_ReleaseGPUBuffer(device, vBuffer);
+  SDL_Quit();
 }
-
---
+++ src/testing.cpp
@@ -0,0 +1,84 @@
+// #define SDL_MAIN_USE_CALLBACKS
+// #include <SDL3/SDL_stdinc.h>
+// #include <SDL3/SDL_timer.h>
+// #include <SDL3/SDL.h>
+// #include <SDL3/SDL_gpu.h>
+// #include <SDL3/SDL_init.h>
+// #include <SDL3/SDL_main.h>
+// #include <SDL3/SDL_video.h>
+//
+// //init window and gpu
+// Uint32 width, height;
+// SDL_Window* window;
+// SDL_GPUDevice* device;
+// SDL_GPUTexture* swapTexture;
+// SDL_GPURenderPass* renderPass;
+// SDL_GPUCommandBuffer* cmd;
+//
+// SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
+//   //creating window
+//   window = SDL_CreateWindow("Hello maybe triangle", 720, 480, 0);
+//
+//   //creating device
+//   device = SDL_CreateGPUDevice(SDL_GPU_SHADERFORMAT_SPIRV, false, NULL);
+//   SDL_ClaimWindowForGPUDevice(device, window);
+//
+//   return SDL_APP_CONTINUE;
+// }
+//
+// SDL_AppResult SDL_AppIterate(void *appstate) {
+//   //creating command buffer (gpu command array)
+//   cmd = SDL_AcquireGPUCommandBuffer(device);
+//
+//
+//   //get the swapchain texture
+//   SDL_WaitAndAcquireGPUSwapchainTexture(cmd, window, &swapTexture, &width, &height);
+//
+//   //end fram early if no swapchain texture
+//   if(swapTexture == NULL) {
+//     //always submint the command buffer after new instructions
+//     SDL_SubmitGPUCommandBuffer(cmd);
+//     return SDL_APP_CONTINUE;
+//   }
+//
+//   //get ticks and convert from ms to s
+//   const double tick = ((double)SDL_GetTicks()) / 1000.0;
+//   //make sine wave between each color using tick as basis
+//   const float red = (float) (0.7 + 0.7 * SDL_sin(tick));
+//   const float blue = (float) (0.7 + 0.7 * SDL_sin(tick + SDL_PI_D * 2 / 3));
+//   const float green = (float) (0.7 + 0.7 * SDL_sin(tick + SDL_PI_D * 4 / 3));
+//
+//   //create the color target                                     1
+//   SDL_GPUColorTargetInfo colorTargetInfo{};
+//   colorTargetInfo.clear_color = {red, blue, green, 255/255.0f};
+//   colorTargetInfo.load_op = SDL_GPU_LOADOP_CLEAR;
+//   colorTargetInfo.store_op = SDL_GPU_STOREOP_STORE;
+//   colorTargetInfo.texture = swapTexture;
+//
+//   //begin render pass
+//   renderPass = SDL_BeginGPURenderPass(cmd, &colorTargetInfo, 1, NULL);
+//
+//   //drawing
+//
+//   //end render pass
+//   SDL_EndGPURenderPass(renderPass);
+//
+//   //sending command buffer to gpu
+//   SDL_SubmitGPUCommandBuffer(cmd);
+//
+//   return SDL_APP_CONTINUE;
+// }
+//
+// SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
+//   if(event->type == SDL_EVENT_WINDOW_CLOSE_REQUESTED) {
+//     return SDL_APP_SUCCESS;
+//   }
+//   return SDL_APP_CONTINUE;
+// }
+//
+// void SDL_AppQuit(void *appstate, SDL_AppResult result) {
+//   //destroying device and window
+//   SDL_DestroyGPUDevice(device);
+//   SDL_DestroyWindow(window);
+// }
+
